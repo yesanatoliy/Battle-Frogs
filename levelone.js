d398db39722f1346ca7a1c8f1af03e7883734569
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-01-23 16:53:06 -0500

    added some global variables to level one, added event listeners to attack options

--- levelone.js
+++ levelone.js
@@ -1,4 +1,28 @@
+//global functions
+let playerHealth = 100
+let enemyHealth = 25
+const slashButton = document.querySelector("#slash")
+const shootButton = document.querySelector("#shoot")
+const punchButton = document.querySelector("#punch")
+
+console.log(slashButton, shootButton, punchButton)
+//functions
 const showIntro = () => {
     document.querySelector('.intro').style.opacity = 1
 }
 showIntro()
+
+
+//event listeners
+
+slashButton.addEventListener('click', () => {
+    console.log(slashButton)
+})
+
+shootButton.addEventListener('click', () => {
+    console.log(slashButton)
+})
+
+punchButton.addEventListener('click', () => {
+    console.log(slashButton)
+})
