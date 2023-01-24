a95ef4fcc32f2a6a96bd47213923a9c176989947
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-01-23 23:57:46 -0500

    made HP displays, also made a div for the game screen aka the game board so i can prepend dialogue when i need to without putting it in the grid container

--- levelone.js
+++ levelone.js
@@ -4,13 +4,22 @@ let enemyHealth = 25
 const slashButton = document.querySelector("#slash")
 const shootButton = document.querySelector("#shoot")
 const punchButton = document.querySelector("#punch")
+const playerHealthDisplay = document.getElementById("playerHealthDisplay")
+playerHealthDisplay.innerText = playerHealth
+const enemyHealthDisplay = document.getElementById("enemyHealthDisplay")
+enemyHealthDisplay.innerText = enemyHealth
+const gameScreen = document.querySelector("#game-screen")
 
 console.log(slashButton, shootButton, punchButton)
 //functions
+
 const showIntro = () => {
-    document.querySelector('.intro').style.opacity = 1
+    let intro = document.createElement('p')
+    intro.innerText = `Enemy: "Hey, what're you doing here??"`
+    gameScreen.prepend(intro)
 }
 showIntro()
+
 
 
 //event listeners
