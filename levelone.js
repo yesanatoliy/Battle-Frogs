6f8bdd671b07efd36d6c4a901ac1ca4fec5122e6
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-01-24 13:37:30 -0500

    started battle functions including chooseSlash updateHealth and randomChoice

--- levelone.js
+++ levelone.js
@@ -1,6 +1,8 @@
 //global functions
 let playerHealth = 100
 let enemyHealth = 25
+let playerChoice
+let enemyChoice
 const slashButton = document.querySelector("#slash")
 const shootButton = document.querySelector("#shoot")
 const punchButton = document.querySelector("#punch")
@@ -20,12 +22,46 @@ const showIntro = () => {
 }
 showIntro()
 
+const randomChoice = () => {
+    if(Math.floor(Math.random()*10) <= 3){
+        enemyChoice = "slash"
+    }
+    else if (Math.floor(Math.random()*10) > 3 && Math.floor(Math.random()*10) <= 6){
+        enemyChoice = "shoot"
+    }
+    else{
+        enemyChoice = "punch"
+    }
+}
+
+const updateHealth = () => {
+    if(playerChoice === enemyChoice){
+        enemyHealth -= 5
+        playerHealth -= 5
+    }
+    else if(playerChoice === "slash" && enemyChoice === "shoot"){
+        enemyHealth -= 10
+    }
+    else if(playerChoice === "slash" && enemyChoice === "punch"){
+        playerHealth -= 10
+    }
+}
+
+const chooseSlash = () => {
+    playerChoice = "slash"
+    randomChoice()
+    console.log(enemyChoice, playerChoice)
+
+}
+
+
 
 
 //event listeners
 
 slashButton.addEventListener('click', () => {
     console.log(slashButton)
+    chooseSlash()
 })
 
 shootButton.addEventListener('click', () => {
