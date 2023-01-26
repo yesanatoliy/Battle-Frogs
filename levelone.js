e070396b27a3aeb308204e911f9f0591faaebe3a
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-01-26 01:17:18 -0500

    that was a horrible mistake i almost left a condition where if you tie you die in the game because i forgot to edit it out while i was testing out the lose screen

--- levelone.js
+++ levelone.js
@@ -67,7 +67,7 @@ const randomChoice = () => {
 const updateHealth = () => {
     if(playerChoice === enemyChoice){
         enemyHealth -= 5
-        playerHealth -= 100
+        playerHealth -= 5
         battleLog.innerText = `It's a tie! You both chose ${playerChoice} and both took 5 damage.`
     }
     else if(
